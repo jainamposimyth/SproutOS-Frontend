cce9321dca9d135b15ce5d66ede54c10b9e677be
Jainam patel <jainam.posimyth@gmail.com>
2025-11-12 17:27:11 +0530

    added global variables

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
             </div>
             <div className='bg-sprout-color-border-dark-weak   ml-20 md:ml-38 flex w-56 md:w-96 rounded-md'>
-                <div className='md:pl-8  pt-2 ml-0 md:ml-24'>
+                <div className='md:pl-8  pt-2 ml-4 md:ml-24'>
 
                     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path opacity="0.9" d="M15.75 15.75L12.75 12.75M14.25 8.25C14.25 9.8413 13.6179 11.3674 12.4926 12.4926C11.3674 13.6179 9.8413 14.25 8.25 14.25C6.6587 14.25 5.13258 13.6179 4.00736 12.4926C2.88214 11.3674 2.25 9.8413 2.25 8.25C2.25 6.6587 2.88214 5.13258 4.00736 4.00736C5.13258 2.88214 6.6587 2.25 8.25 2.25C9.8413 2.25 11.3674 2.88214 12.4926 4.00736C13.6179 5.13258 14.25 6.6587 14.25 8.25Z" stroke="#D7D3C9" strokeWidth="1.125" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
